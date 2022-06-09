c719195160abc874eff8cd4e05b84a1ab3256075
Cibranix <cccabaleiro19@esei.uvigo.es>
2022-06-09 18:07:22 +0200

    Añadido "x a y de z" (algunas pruebas)

--- IUJULIO/js/paginacion.js
+++ IUJULIO/js/paginacion.js
@@ -1,4 +1,21 @@
-function construyePaginacion(funcion, filasporpagina, total, empieza, filas){
+//Nombre alumno: Cibrán Cores Cabaleiro
+//DNI alumno: 54506586J
+function construyePaginacion(funcion, filasporpagina, total, empieza, filas) {
+    if (total == 0) { //Si hay 0 en la busqueda ocultamos el item paginación
+        $("#itemPaginacion").hide();  // hide itemPaginacion
+        actualizarDetallesPag(totalFilas, primeraFila, filasPagina); //componente id "paginación"
+    
+      } else {
+        $("#itemPaginacion").show();  // show itemPaginacion
+        actualizarDetallesPag(totalFilas, primeraFila, filasPagina);//componente id "paginación"
+    }
+}
 
-
+function actualizarDetallesPag(total, primeraFila, filas) {
+  var ultimaFila = primeraFila + filas;
+  if (total > 0) {
+    primeraFila++;
+  }
+  let paginacion = primeraFila + " a " + ultimaFila + " de " + total;
+  $("#paginacion").html(paginacion);
 }
